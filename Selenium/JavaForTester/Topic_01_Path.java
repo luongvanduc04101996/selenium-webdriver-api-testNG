a171034cb81d608dbfdefe690f8adf69909abf49
DUC <DUC@192.168.2.9>
2020-11-02 21:27:14 +0700

    Add new repository

---
+++ Selenium/JavaForTester/Topic_01_Path.java
@@ -0,0 +1,12 @@
+package JavaForTester;
+
+public class Topic_01_Path {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String projectFolder = System.getProperty("user.dir");
+		System.out.println(projectFolder);
+
+	}
+
+}
---
+++ Selenium/webdriver/Topic2_Browser.java
@@ -0,0 +1,44 @@
+package webdriver;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.annotations.Test;
+
+public class Topic2_Browser {
+	WebDriver driver;
+	String projectFolder = System.getProperty("user.dir");
+	
+	@Test
+	public void TC1_FireFox() {
+		driver = new FirefoxDriver();
+		
+		driver.get("https://google.com");
+		
+		driver.quit();
+	}
+	
+	@Test
+	public void TC2_Chrome() {
+		System.setProperty("webdriver.chrome.driver", projectFolder + "\\BrowserDrivers\\chromedriver.exe");
+		
+		driver = new ChromeDriver();
+		
+		driver.get("https://google.com");
+		
+		driver.quit();
+	}
+	
+	@Test
+	public void TC3_Edge() {
+		System.setProperty("webdriver.edge.driver", ".\\BrowserDrivers\\chromedriver.exe");
+		
+		driver = new EdgeDriver();
+		
+		driver.get("https://google.com");
+		
+		driver.quit();
+	}
+	
+}
