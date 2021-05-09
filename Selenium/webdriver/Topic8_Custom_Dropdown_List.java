f2f35f7c37f512a50d35dd769c3bd4b17921edca
Luong Van Duc <luongvanduc04101996@gmail.com>
2021-05-09 22:44:41 +0700

    Button_CheckBox_RadioButton DONE

--- Selenium/webdriver/Topic8_Custom_Dropdown_List.java
+++ Selenium/webdriver/Topic8_Custom_Dropdown_List.java
@@ -2,76 +2,149 @@ package webdriver;
 
 import org.testng.annotations.Test;
 
-
 import org.testng.annotations.BeforeClass;
 
 import java.util.List;
 import java.util.concurrent.TimeUnit;
 
 import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
-import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.Assert;
 import org.testng.annotations.AfterClass;
 
 public class Topic8_Custom_Dropdown_List {
 	WebDriver driver;
 	WebDriverWait explicitWait;
+	String projectFolder = System.getProperty("user.dir");
+	JavascriptExecutor jvScripExecutor;
+
+	@BeforeClass
+	public void beforeClass() {
+		System.setProperty("webdriver.chrome.driver", projectFolder + "\\BrowserDrivers\\chromedriver.exe");
+		driver = new ChromeDriver();
+		explicitWait = new WebDriverWait(driver, 30);
+
+		driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+		driver.manage().window().maximize();
+
+		jvScripExecutor = (JavascriptExecutor) driver;
+	}
 	
-  @BeforeClass
-  public void beforeClass() {
-	  driver = new FirefoxDriver();
-	  explicitWait = new WebDriverWait(driver, 30);
-	  
-	  driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
-	  driver.manage().window().maximize();
-  }
-  @Test
-  public void TC1_JQuery() {
-	  driver.get("http://jqueryui.com/resources/demos/selectmenu/default.html");
-	  selectItemInCustomDropdown("//span[@id='number-button']", "//ul[@id='number-menu']//div", "4");
-	  sleepInSecond(3);
-	  
-	  selectItemInCustomDropdown("//span[@id='number-button']", "//ul[@id='number-menu']//div", "10");
-	  sleepInSecond(3);
-	  
-	  selectItemInCustomDropdown("//span[@id='number-button']", "//ul[@id='number-menu']//div", "15");
-	  sleepInSecond(3);
-  }
-  public void sleepInSecond(long timeInSecond) {
-	  try {
-		Thread.sleep(timeInSecond*1000);
-	} catch (InterruptedException e) {
-		// TODO Auto-generated catch block
-		e.printStackTrace();
+	@Test
+	public void TC1_JQuery() {
+		driver.get("http://jqueryui.com/resources/demos/selectmenu/default.html");
+		selectItemInCustomDropdown("//span[@id='number-button']", "//ul[@id='number-menu']//div", "4");
+		sleepInSecond(3);
+
+		selectItemInCustomDropdown("//span[@id='number-button']", "//ul[@id='number-menu']//div", "10");
+		sleepInSecond(3);
+
+		selectItemInCustomDropdown("//span[@id='number-button']", "//ul[@id='number-menu']//div", "15");
+		sleepInSecond(2);
 	}
-  }
-  
-  public void selectItemInCustomDropdown(String parentXpath, String allItemXpath, String expectedValueItem) {
-	//  1 - Click vào 1 element (parenXpath) để cho nó xổ hết tất cả các item ra
-	  driver.findElement(By.xpath(parentXpath)).click();
-	//  2 - Chờ all item được load
-	  explicitWait.until(ExpectedConditions.presenceOfAllElementsLocatedBy(By.xpath(allItemXpath)));
-	//  3 - Lưu nó vào 1 List chứa các element
-	  List<WebElement> allItems = driver.findElements(By.xpath(allItemXpath));
-	//  4 - Lấy ra text của từng elemet
-	  for (WebElement item : allItems) {
-		String actualValueItem = item.getText();
-	//  5 - Kiểm tra nó có bằng với text cần tìm hay ko
-		if (actualValueItem.equals(expectedValueItem)) {
-		//  6 - Nếu như có thì click vào - thoát khỏi vòng lập
-			item.click();
-			break;
+	
+	@Test
+	public void TC2_Angular() {
+		driver.get("https://ej2.syncfusion.com/angular/demos/?_ga=2.262049992.437420821.1575083417-524628264.1575083417#/material/drop-down-list/data-binding");
+
+		selectItemInCustomDropdown("//ejs-dropdownlist[@id='games']//span[contains(@class,'e-search-icon')]",
+				"//ul[@id='games_options']/li", "Badminton");
+		sleepInSecond(2);
+		Assert.assertEquals(getSelectedValueAngular(), "Badminton");
+
+		selectItemInCustomDropdown("//ejs-dropdownlist[@id='games']//span[contains(@class,'e-search-icon')]",
+				"//ul[@id='games_options']/li", "Basketball");
+		sleepInSecond(2);
+	}
+	
+	@Test
+	public void TC3_React() {
+		driver.get("https://react.semantic-ui.com/maximize/dropdown-example-selection/");
+
+		selectItemInCustomDropdown("//i[@class='dropdown icon']", "//div[@class='visible menu transition']//span",
+				"Matt");
+		sleepInSecond(2);
+		Assert.assertTrue(driver.findElement(By.xpath("//div[text()='Matt']")).isDisplayed());
+	}
+	
+	@Test
+	public void TC4_VueJS() {
+		driver.get("https://mikerodham.github.io/vue-dropdowns/");
+		selectItemInCustomDropdown("//span[@class='caret']", "//ul[@class='dropdown-menu']//a", "Second Option");
+		sleepInSecond(2);
+		Assert.assertEquals(driver.findElement(By.xpath("//li[@class='dropdown-toggle']")).getText(), "Second Option");
+	}
+	
+	@Test
+	public void TC5_Editdropdown() {
+		driver.get("https://react.semantic-ui.com/maximize/dropdown-example-search-selection/");
+		selectItemInEditDropdown("//input[@class='search']","//div[@role='listbox']//span" , "Andorra");
+		sleepInSecond(2);
+		Assert.assertEquals(driver.findElement(By.xpath("//div[@role='alert']")).getText(), "Andorra");
+	}
+
+	public String getSelectedValueAngular() {
+		return (String) jvScripExecutor
+				.executeScript("return document.querySelector(\"select[name='games'] option\").text");
+	}
+
+	public void sleepInSecond(long timeInSecond) {
+		try {
+			Thread.sleep(timeInSecond * 1000);
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
 		}
-	//  Nếu ko có thì tiếp tục duyệt cho tới khi hết all item
 	}
-  }
-  
-  @AfterClass
-  public void afterClass() {
-	  driver.quit();
-  }
+
+	public void selectItemInCustomDropdown(String parentXpath, String allItemXpath, String expectedValueItem) {
+		// 1 - Click vào 1 element (parenXpath) để cho nó xổ hết tất cả các item ra
+		driver.findElement(By.xpath(parentXpath)).click();
+		// 2 - Chờ all item được load
+		explicitWait.until(ExpectedConditions.presenceOfAllElementsLocatedBy(By.xpath(allItemXpath)));
+		// 3 - Lưu nó vào 1 List chứa các element
+		List<WebElement> allItems = driver.findElements(By.xpath(allItemXpath));
+		// 4 - Lấy ra text của từng elemet
+		for (WebElement item : allItems) {
+			String actualValueItem = item.getText();
+			// 5 - Kiểm tra nó có bằng với text cần tìm hay ko
+			if (actualValueItem.equals(expectedValueItem)) {
+				// 6 - Nếu như có thì click vào - thoát khỏi vòng lập
+				item.click();
+				break;
+			}
+			// Nếu ko có thì tiếp tục duyệt cho tới khi hết all item
+		}
+	}
+	
+	public void selectItemInEditDropdown(String parentXpath, String allItemXpath, String expectedValueItem) {
+		// 1 - Input dữ liệu
+		driver.findElement(By.xpath(parentXpath)).sendKeys(expectedValueItem);;
+		// 2 - Chờ all item được load
+		explicitWait.until(ExpectedConditions.presenceOfAllElementsLocatedBy(By.xpath(allItemXpath)));
+		// 3 - Lưu nó vào 1 List chứa các element
+		List<WebElement> allItems = driver.findElements(By.xpath(allItemXpath));
+		// 4 - Lấy ra text của từng elemet
+		for (WebElement item : allItems) {
+			String actualValueItem = item.getText();
+			// 5 - Kiểm tra nó có bằng với text cần tìm hay ko
+			if (actualValueItem.equals(expectedValueItem)) {
+				// 6 - Nếu như có thì click vào - thoát khỏi vòng lập
+				item.click();
+				break;
+			}
+			// Nếu ko có thì tiếp tục duyệt cho tới khi hết all item
+		}
+	}
+
+	@AfterClass
+	public void afterClass() {
+		driver.quit();
+	}
 
 }
---
+++ Selenium/webdriver/Topic9_Button_RadioButton_CheckBox_Alert.java
@@ -0,0 +1,153 @@
+package webdriver;
+
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+import java.util.concurrent.TimeUnit;
+
+import org.junit.Assert;
+import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterClass;
+
+public class Topic9_Button_RadioButton_CheckBox_Alert {
+	String projectFolder = System.getProperty("user.dir");
+	WebDriver driver;
+	By userBy = By.id("login_username");
+	By passwordBy = By.id("login_password");
+	By buttonLogin = By.className("fhs-btn-login");
+	By loginTab = By.xpath("//a[text()='Đăng nhập']");
+
+	JavascriptExecutor jvscrip;
+
+	@BeforeClass
+	public void beforeClass() {
+		System.setProperty("webdriver.chrome.driver", projectFolder + "\\BrowserDrivers\\chromedriver.exe");
+		driver = new ChromeDriver();
+
+		driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+		driver.manage().window().maximize();
+
+		jvscrip = (JavascriptExecutor) driver;
+	}
+
+	public void TC1_Button() {
+		driver.get("https://www.fahasa.com/customer/account/create");
+		driver.findElement(loginTab).click();
+		Assert.assertFalse(isElementEnabled(buttonLogin));
+		driver.findElement(userBy).sendKeys("lvd@gmail.com");
+		driver.findElement(passwordBy).sendKeys("123456");
+		sleepInSeconds(3);
+		Assert.assertTrue(isElementEnabled(buttonLogin));
+		driver.navigate().refresh();
+		driver.findElement(loginTab).click();
+		removeDisableAttribute(buttonLogin);
+		driver.findElement(buttonLogin).click();
+		sleepInSeconds(3);
+		Assert.assertEquals(driver.findElement(By.xpath("//input[@id='login_username']/parent::div/following-sibling::div")).getText(),"Thông tin này không thể để trống");
+		Assert.assertEquals(driver.findElement(By.xpath("//input[@id='login_password']/parent::div/following-sibling::div")).getText(),"Thông tin này không thể để trống");
+	}
+
+	public void TC2_Checkbox_Radio() {
+		driver.get("http://demos.telerik.com/kendo-ui/styling/checkboxes");
+		checkToCheckBoxOrRadio(driver.findElement(By.xpath("//label[text()='Dual-zone air conditioning']/preceding-sibling::input")));
+		Assert.assertTrue(isElementSelected(driver.findElement(By.xpath("//label[text()='Dual-zone air conditioning']/preceding-sibling::input"))));
+		sleepInSeconds(2);
+		uncheckToCheckBox(driver.findElement(By.xpath("//label[text()='Dual-zone air conditioning']/preceding-sibling::input")));
+		Assert.assertFalse(isElementSelected(driver.findElement(By.xpath("//label[text()='Dual-zone air conditioning']/preceding-sibling::input"))));
+		sleepInSeconds(2);
+		driver.get("http://demos.telerik.com/kendo-ui/styling/radios");
+		checkToCheckBoxOrRadio(driver.findElement(By.xpath("//label[text()='2.0 Petrol, 147kW']/preceding-sibling::input")));
+		Assert.assertTrue(isElementSelected(driver.findElement(By.xpath("//label[text()='2.0 Petrol, 147kW']/preceding-sibling::input"))));
+		sleepInSeconds(2);
+		uncheckToCheckBox(driver.findElement(By.xpath("//label[text()='2.0 Petrol, 147kW']/preceding-sibling::input")));
+		Assert.assertTrue(isElementSelected(driver.findElement(By.xpath("//label[text()='2.0 Petrol, 147kW']/preceding-sibling::input"))));
+		sleepInSeconds(2);
+	}
+
+	@Test
+	public void TC3_CustomCheckboxOrRadio() {
+		driver.get("https://material.angular.io/components/radio/examples");
+		clickByJavaScript(driver.findElement(By.xpath("//input[@value='Summer']")));
+		Assert.assertTrue(isElementSelected(driver.findElement(By.xpath("//input[@value='Summer']"))));
+		sleepInSeconds(2);
+		
+		driver.get("https://material.angular.io/components/checkbox/examples");
+		clickByJavaScript(driver.findElement(By.xpath("//span[text()='Checked']/preceding-sibling::span/input")));
+		sleepInSeconds(2);
+		clickByJavaScript(driver.findElement(By.xpath("//span[text()='Indeterminate']/preceding-sibling::span/input")));
+		sleepInSeconds(2);
+		Assert.assertTrue(isElementSelected(driver.findElement(By.xpath("//span[text()='Checked']/preceding-sibling::span/input"))));
+		Assert.assertTrue(isElementSelected(driver.findElement(By.xpath("//span[text()='Indeterminate']/preceding-sibling::span/input"))));
+		
+		clickByJavaScript(driver.findElement(By.xpath("//span[text()='Checked']/preceding-sibling::span/input")));
+		sleepInSeconds(2);
+		clickByJavaScript(driver.findElement(By.xpath("//span[text()='Indeterminate']/preceding-sibling::span/input")));
+		sleepInSeconds(2);
+		Assert.assertFalse(isElementSelected(driver.findElement(By.xpath("//span[text()='Checked']/preceding-sibling::span/input"))));
+		Assert.assertFalse(isElementSelected(driver.findElement(By.xpath("//span[text()='Indeterminate']/preceding-sibling::span/input"))));
+		
+		Assert.assertTrue(isElementSelected(driver.findElement(By.xpath("//span[text()='After']/preceding-sibling::span/input"))));
+		Assert.assertFalse(isElementSelected(driver.findElement(By.xpath("//span[text()='Before']/preceding-sibling::span/input"))));
+		//checkToCheckBoxOrRadio(driver.findElement(By.xpath("//span[text()='Before']/preceding-sibling::span/input")));
+		clickByJavaScript(driver.findElement(By.xpath("//span[text()='Before']/preceding-sibling::span/input")));
+		sleepInSeconds(3);
+		Assert.assertTrue(isElementSelected(driver.findElement(By.xpath("//span[text()='Before']/preceding-sibling::span/input"))));
+	}
+
+	@AfterClass
+	public void afterClass() {
+		driver.quit();
+	}
+
+	public void checkToCheckBoxOrRadio(WebElement element) {
+		if (!element.isSelected()) {
+			element.click();
+		}
+	}
+
+	public void uncheckToCheckBox(WebElement element) {
+		if (element.isSelected()) {
+			element.click();
+		}
+	}
+
+	public boolean isElementSelected(WebElement element) {
+		if (element.isSelected()) {
+			return true;
+		} else {
+			return false;
+		}
+	}
+
+	public void sleepInSeconds(long timeInSecond) {
+		try {
+			Thread.sleep(timeInSecond * 1000);
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+	
+	public boolean isElementEnabled(By by) {
+		if (driver.findElement(by).isEnabled()) {
+			return true;
+		} else {
+			return false;
+		}
+
+	}
+	
+	public void removeDisableAttribute(By by) {
+		WebElement element = driver.findElement(by);
+		jvscrip.executeScript("arguments[0].removeAttribute('disabled')", element);
+	}
+	
+	public void clickByJavaScript(WebElement element) {
+		jvscrip.executeScript("arguments[0].click();", element);
+	}
+
+}
